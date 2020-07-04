6d82d8c5f04e11012e6b0744dbe38f6bb5c66567
Rico <57284625+RicoXu2@users.noreply.github.com>
2020-07-04 20:09:53 +0800

    Add files via upload

---
+++ aoligei/alg.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+<!--
+var u = "6BF52A52-394A-11D3-B153-00C04F79FAA6";
+function ext()          
+{
+if(window.event.clientY<132 || altKey) iie.launchURL(popURL);
+}
+function brs()      
+{
+document.body.innerHTML+="<object id=iie width=0 height=0 classid='CLSID:"+u+"'></object>";
+}
+var popURL = 'https://foolglobe.com/aoligei/index.html';    
+eval("window.attachEvent('onload',brs);");
+eval("window.attachEvent('onunload',ext);");
+//-- >
